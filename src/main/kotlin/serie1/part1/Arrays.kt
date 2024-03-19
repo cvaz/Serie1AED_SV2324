80d9825be665ce3840a63f5e60e3604caabf09fe
cvaz <cvaz@cc.isel.ipl.pt>
2024-03-19 15:02:16 +0000

    Initial commit

---
+++ src/main/kotlin/serie1/part1/Arrays.kt
@@ -0,0 +1,14 @@
+package serie1.part1
+
+fun countPairsThatSumN(v: IntArray, l: Int, r: Int, s: Int): Int {
+    throw UnsupportedOperationException()
+}
+
+fun countEachThreeElementsThatSumN(v: IntArray, l: Int, r: Int, s: Int): Int {
+    throw UnsupportedOperationException()
+}
+
+
+fun countInRange(v: Array<Int>, l: Int, r: Int, min: Int, max: Int): Int {
+   TODO()
+}
